8f5a8042e5baae60c48ae1e87e4a734ffc2621cc
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-08-05 16:17:00 +0200

    Added styles to tweets, added functions to handle clicks and create new tweets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import {tweetsData} from "./data.js"
-
+import {nanoid} from "nanoid"
+import scrimbalogo from "./images/scrimbalogo.png"
 import Tweet from "./components/Tweet"
 
 
@@ -9,34 +10,61 @@ function App() {
   const [tweets, setTweets] = useState(tweetsData)
 
   // tailwind styles
-    const container = `flex flex-col p-3 w-11/12 items-center`
-    const title = `text-2xl font-bold text-sky-500 mr-auto`
-    const header = `w-11/12`
+    const container = `flex flex-col p-3 w-11/12 items-center mx-auto `
+    const title = `text-3xl font-bold text-sky-500 mr-auto`
+    const header = `w-11/12 flex flex-col items-center`
     const input = `border border-neutral-800 w-full rounded-lg shadow-lg shadow-neutral-800/30
-                   my-3 p-2 
+                   my-5 p-2 bg-neutral-50
                    focus:bg-neutral-200 focus:outline-none focus:ring-2 focus:ring-sky-500
                    focus:ring-offset-white focus:text-neutral-900 focus:font-semibold
                    transition-colors duration-200 ease-in-out`
     const btnTweet = `bg-gradient-to-br from-sky-500 to-sky-400 text-neutral-50 font-bold
                       rounded-full shadow-lg shadow-neutral-800/30 py-3 px-5 text-lg 
                       hover:transform hover:scale-105 active:scale-95 w-full`
-    const main = ``
+    const main = `flex flex-col items-center gap-3 mt-6 w-full`
 
   //
 
   const tweetsArray = tweets.map(t => (
-    <Tweet data={t} key={t.uuid}/>
+    <Tweet 
+      data={t} 
+      key={t.uuid} 
+      like={() => handleLikes(t.uuid)} 
+      retweet={() => handleRetweets(t.uuid)}
+      isLiked={t.isLiked}
+      isRetweeted={t.isRetweeted}
+    />
   ))
 
-  
+  const handleLikes = uuid => {
+    setTweets(t => t.map(
+      t => t.uuid === uuid ? {...t, likes : t.likes + (t.isLiked ? -1 : +1 ), isLiked : !t.isLiked} : t))
+  }
+  const handleRetweets = uuid => {
+    setTweets(t => t.map(
+      t => t.uuid === uuid ? {...t, retweets : t.retweets + (t.isRetweeted ? -1 : +1), isRetweeted : !t.isRetweeted} : t))
+  }
 
   const handleChange = e => setText(e.target.value) 
-  const handleClick = () => console.log(text)
+  const handleClick = () => {
+    setTweets(t => [...t, {
+      handle: `@modarator`,
+      profilePic: scrimbalogo,
+      likes: 0,
+      retweets: 0,
+      tweetText: text,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: nanoid(),
+    }])
+    setText("")
+  }
 
   return (
    <div className={container}>
-    <h1 className={title}>Twitter</h1>
     <header className={header}>
+      <h1 className={title}>Twitter</h1>
       <textarea
         rows={5}
         className={input}
@@ -44,7 +72,7 @@ function App() {
         onChange={handleChange} 
         aria-label="Write your tweet here"
         placeholder="Write your tweet here..."></textarea>
-      <button className={btnTweet} onClick={() => handleClick()}>Tweet</button>
+      <button className={btnTweet} onClick={() => handleClick()} disabled={!text}>Tweet</button>
     </header>
     <main className={main}>
       {tweetsArray}
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -2,16 +2,20 @@ import { FaHeart } from 'react-icons/fa';
 import { FaRetweet } from 'react-icons/fa';
 import { FaRegCommentDots } from 'react-icons/fa';
 
-export default function Tweet({data}){
-    const container = `flex`
-    const img = `rounded-full w-14 h-14`
-    const infoSetion = ``
-    const handle = ``
-    const tweetText = ``
-    const btnContainer = `flex`
-    const btnSection = ``
-    const btn = ``
-    const icon = ``
+export default function Tweet({data, like, retweet, isLiked, isRetweeted}){
+    const container = `flex gap-2 py-4 pl-2 pr-6 text-neutral-700 w-full
+                       shadow-lg shadow-neutral-800/30 rounded-lg bg-neutral-50`
+    const img = `rounded-full w-18 h-18`
+    const infoSetion = `flex flex-col w-full`
+    const handle = `text-sm`
+    const tweetText = `mb-4 mt-2 font-semibold`
+    const btnContainer = `flex gap-3 w-10/12`
+    const btnSection = `flex flex-1 w-`
+    const btn = `mr-1 `
+    const icon = `h-6 w-6 hover:transform hover:scale-115 active:scale-90`
+    const repliesIcon = `text-neutral-500`
+    const heartIcon = `${isLiked ? "text-rose-500" : "text-neutral-500"}`
+    const retweetIcon = `${isRetweeted ? "text-lime-500" : "text-neutral-500"}`
     const amount = ``
     return(
         <section className={container}>
@@ -21,16 +25,16 @@ export default function Tweet({data}){
                 <p className={tweetText}>{data.tweetText}</p>
                 <div className={btnContainer}>
                     <span className={btnSection}>
-                        <button className={btn}><FaRegCommentDots /></button>
-                        <p className={amount}></p>
+                        <button className={btn}><FaRegCommentDots className={`${icon} ${repliesIcon}`} /></button>
+                        <p className={amount}>{data.replies.length}</p>
                     </span>
                     <span className={btnSection}>
-                        <button className={btn}><FaHeart /></button>
-                        <p className={amount}></p>
+                        <button className={btn} onClick={like}><FaHeart className={`${icon} ${heartIcon}`}/></button>
+                        <p className={amount}>{data.likes}</p>
                     </span>
                     <span className={btnSection}>
-                        <button className={btn}><FaRetweet /></button>
-                        <p className={amount}></p>
+                        <button className={btn} onClick={retweet}><FaRetweet className={`${icon} ${retweetIcon}`}/></button>
+                        <p className={amount}>{data.retweets}</p>
                     </span>
                 </div>
             </section>
